4724097fd124ce53d4612f8a5de12e845cd5b61d
qmr1cob <saranishkumar11@gmail.com>
2024-03-28 18:03:45 +0530

    fix

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 # script.py
-
+import numpy as np
 def main():
     print("Hello from Python script!")
+    array = np.array([1, 2, 3, 4, 5])
+
+# Printing the array
+    print("Original array:", array)
 
 if __name__ == "__main__":
     main()
